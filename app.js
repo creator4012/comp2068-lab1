857a0a19e8a6544141b7f41795345763dd6cdb3e
Sahilpreet Singh <41137239+Sahilpreet4012@users.noreply.github.com>
2019-05-26 17:55:16 -0400

    Lab1 done
    Completed Lab 1

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hi, welcome to our group lab 1. You can navigate through our site by typing /about, /contact or just / in the URL.');
+});
+
+app.get('/about', (req, res) => {
+    res.send('Our group consist of 3 members. Sahilpreet Singh, Gurkirat Singh and Anandpal Singh Sandhu');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('You can reach us at admin@media.ca or visit us in orillia.');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
